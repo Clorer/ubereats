d7ea4b2171ab6ec7ccb592fb31a73ff577eaffb2
Clorer <123593518+Clorer@users.noreply.github.com>
2025-01-17 19:52:09 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 console.log("HELLO");
 const buttons = document.querySelectorAll('.dishes_item'); // Находим все кнопки с классом "my-button"
 document.getElementById("start_button").classList.add('active');
+const logo = document.getElementById("main_logo");
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
@@ -16,4 +17,6 @@ cards.forEach(card => {
     });
 });
     
- 
+logo.addEventListener('click', () => {
+  window.open('restaurant.html');
+    });
